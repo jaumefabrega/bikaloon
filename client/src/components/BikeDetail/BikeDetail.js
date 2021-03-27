10f42c709dbf62df9ac5661e368e0732013053c7
Jaume Fabrega <jaumefabregaroda@gmail.com>
2021-03-27 15:17:44 +0100

    feat CLIENT: basic BikeDetail

--- client/src/components/BikeDetail/BikeDetail.js
+++ client/src/components/BikeDetail/BikeDetail.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { useParams } from "react-router-dom";
+import { bicycles } from '../../mockData/bicycles';
+import "./BikeDetail.css"
 
 export default function BikeDetail() {
   let { bikeId } = useParams();
+  bikeId = parseInt(bikeId);
+
+  const bike = bicycles.filter(bike => bike.id === bikeId)[0];
+
   return (
-    <div>
+    <div className="product-detail">
       Bike detail for bike with id {bikeId}
+      <h1>{bike.name}</h1>
+      <img src={bike.image} alt="bike" />
+      <p>{bike.description}</p>
     </div>
   )
 }
