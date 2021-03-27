65843a6f5713466dae48ae00459f1c27dc9781d1
Jaume Fabrega <jaumefabregaroda@gmail.com>
2021-03-27 15:48:23 +0100

    feat CLIENT: add complements mock data for dev

--- client/src/mockData/bicycles.js
+++ client/src/mockData/bicycles.js
@@ -4,20 +4,47 @@ export const bicycles = [
     "name": "Lance Armstrong bike",
     "price": 1300,
     "description": "A very cool american bike",
-    "image": "https://medias.maisonsdumonde.com/image/upload/q_auto,f_auto/w_1000/img/decoracion-de-pared-con-bicicleta-de-metal-negro-186x108-1000-3-1-164903_1.jpg"
+    "image": "https://medias.maisonsdumonde.com/image/upload/q_auto,f_auto/w_1000/img/decoracion-de-pared-con-bicicleta-de-metal-negro-186x108-1000-3-1-164903_1.jpg",
+    "complementOptions": [
+      {"id": 1, "complementId": 1, "value":"17 inches"},
+      {"id": 2, "complementId": 1, "value":"19 inches"},
+      {"id": 3, "complementId": 2, "value":"red"},
+      {"id": 4, "complementId": 2, "value":"blue"},
+    ],
+    "complementCombinationExclusions": [
+      "1-3"
+    ]
   },
   {
     "id": 2,
     "name": "Valverde",
     "price": 750,
     "description": "With clembuterol",
-    "image": "https://contents.mediadecathlon.com/p1633470/k$29c282707c6d811afcedc81b08c59e6e/sq/bicicleta-de-montana-rockrider-st-100-aluminio-275-21v-amarillo.jpg?format=auto&f=864x864"
+    "image": "https://contents.mediadecathlon.com/p1633470/k$29c282707c6d811afcedc81b08c59e6e/sq/bicicleta-de-montana-rockrider-st-100-aluminio-275-21v-amarillo.jpg?format=auto&f=864x864",
+    "complementOptions": [
+      {"id": 1, "complementId": 1, "value":"17 inches"},
+      {"id": 2, "complementId": 1, "value":"19 inches"},
+      {"id": 3, "complementId": 2, "value":"red"},
+      {"id": 4, "complementId": 2, "value":"blue"},
+    ],
+    "complementCombinationExclusions": [
+      "1-3"
+    ]
   },
   {
     "id": 3,
     "name": "Random biker",
     "price": 123,
     "description": "The cheapest you can find",
-    "image": "https://cdn1.deporvillage.com/cdn-cgi/image/w=900,dpr=1,f=auto,q=75,fit=contain,background=white/product/con-.110845ro_001.jpg"
+    "image": "https://cdn1.deporvillage.com/cdn-cgi/image/w=900,dpr=1,f=auto,q=75,fit=contain,background=white/product/con-.110845ro_001.jpg",
+    "complementOptions": [
+      {"id": 1, "complementId": 1, "value":"17 inches"},
+      {"id": 2, "complementId": 1, "value":"19 inches"},
+      {"id": 3, "complementId": 2, "value":"red"},
+      {"id": 4, "complementId": 2, "value":"blue"},
+    ],
+    "complementCombinationExclusions": [
+      "1-3"
+    ]
   }
 ];
