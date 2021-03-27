cde9c21e16a5232356c435fc42c922440d2d91c1
Jaume Fabrega <jaumefabregaroda@gmail.com>
2021-03-27 14:48:04 +0100

    feat CLIENT: add mock data for development

--- client/src/components/BikesList/BikesList.js
+++ client/src/components/BikesList/BikesList.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { bicycles } from '../../mockData/bicycles';
 
 export default function BikesList() {
   return (
     <div>
       list of bikes
+      { bicycles.map(bicycle => <p>{bicycle.name}</p>)}
     </div>
   )
 }
---
+++ client/src/mockData/bicycles.js
@@ -0,0 +1,23 @@
+export const bicycles = [
+  {
+    "id": 1,
+    "name": "Lance Armstrong bike",
+    "price": 1300,
+    "description": "A very cool american bike",
+    "image": "https://medias.maisonsdumonde.com/image/upload/q_auto,f_auto/w_1000/img/decoracion-de-pared-con-bicicleta-de-metal-negro-186x108-1000-3-1-164903_1.jpg"
+  },
+  {
+    "id": 2,
+    "name": "Valverde",
+    "price": 750,
+    "description": "With clembuterol",
+    "image": "https://contents.mediadecathlon.com/p1633470/k$29c282707c6d811afcedc81b08c59e6e/sq/bicicleta-de-montana-rockrider-st-100-aluminio-275-21v-amarillo.jpg?format=auto&f=864x864"
+  },
+  {
+    "id": 3,
+    "name": "Random biker",
+    "price": 123,
+    "description": "The cheapest you can find",
+    "image": "https://cdn1.deporvillage.com/cdn-cgi/image/w=900,dpr=1,f=auto,q=75,fit=contain,background=white/product/con-.110845ro_001.jpg"
+  }
+];
