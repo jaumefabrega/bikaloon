f86d33ce52424f5dabc9bfbd2e4ff6f696648ea9
Jaume Fabrega <jaumefabregaroda@gmail.com>
2021-03-27 14:58:15 +0100

    feat CLIENT: add BikeListItem

---
+++ client/src/components/BikeListItem/BikeListItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import "./BikeListItem.css"
+
+export default function BikeListItem({ bike }) {
+  return (
+    <div class="list-item">
+      <Link to={`/bike/${bike.id}`} >
+        <h1>{bike.name}</h1>
+        <img src={bike.image} alt="bike" />
+        <p>{bike.description}</p>
+      </Link>
+    </div>
+  )
+}
--- client/src/components/BikesList/BikesList.js
+++ client/src/components/BikesList/BikesList.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { bicycles } from '../../mockData/bicycles';
+import BikeListItem from '../BikeListItem/BikeListItem';
 
 export default function BikesList() {
   return (
     <div>
       list of bikes
-      { bicycles.map(bicycle => <p>{bicycle.name}</p>)}
+      { bicycles.map(bicycle => <BikeListItem bike={bicycle} />)}
     </div>
   )
 }
